fixed average smoothing in test_data

contrastive_learning/tests/test_data.py
@@ -96,47 +96,41 @@ class AnimateMarkers:
         
         print(f'len(self.ids): {len(self.ids)}, len(self.commands): {len(self.commands)}')
 
-        self.camera_intrinsics = np.array([[612.82019043,   0.        , 322.14050293],
-                              [  0.        , 611.48303223, 247.9083252 ],
-                              [  0.        ,   0.        ,   1.        ]])
-        self.distortion_coefficients = np.zeros((5))
+        # Shape: (Frame Length, 2:Box and Dog, 4: 4 corners, 2: [x,y])
+        self.corners_np = np.zeros((len(self.corners), 2, 4, 2))
+        # First dump all the detected markers to corners_np
+        # Put -1 if one of them haven't come into the frame yet
+        for i in range(len(self.corners)):
+            for j in range(len(self.corners[i])): # Number of markers
+                if self.ids[i][j] == 1: # The id check is done here
+                    self.corners_np[i,0,:] = self.corners[i][j][0,:]
+                else:
+                    self.corners_np[i,1,:] = self.corners[i][j][0,:]
 
-        # print(self.corners[0]) # First element of corners will always be non None
 
-        # NOTE: ID Checking should be made
-        # Averaging corners 
-        i = 0
-        j = 1
-        while j < len(self.corners):
-            while j < len(self.corners)-1 and len(self.corners[j]) == 0:
-                j += 1
-            
-            print('i: {}, j: {}'.format(i, j))
-            print('self.ids[i]: {}, self.ids[j]: {}'.format(self.ids[i], self.ids[j]))
-            # Traverse from i to j in self.corners and put each step difference
-            prev_corners = self.corners[i] # We know that both of them at least have one corner 
-            next_corners = self.corners[j] # if one of them has two then second one will be appended to all of them
-            interval = j - i
-            for curr_cor in range(i+1,j):
+        # Put -1 when the dog or the box hasn't come to the frame yet (in the beginning)
+        for i in range(len(self.corners_np)):
+            if self.corners_np[i,1,:].all() == 0: # Only way is the 2nd one is not initialized
+                self.corners_np[i,1,:] = -1
+            else:
+                break
 
-                for k in range(min(len(prev_corners), len(next_corners))):
-                    curr_step = (next_corners[k][0,:] - prev_corners[k][0,:]) / interval 
-                    print('curr_step: {}'.format(curr_step))
+        # print('self.corners_np: {}'.format(self.corners_np))
 
-                    if self.ids[i][k] == self.ids[j][k]: # If the ids are swapped in the middle that makes no sense 
-                        # print(self.corners[curr_cor])
-                        self.corners[curr_cor].append(self.corners[curr_cor-1][k] + curr_step)
-
-                if len(prev_corners) > len(next_corners): # If in ith step there were more detected markers just reflect that marker to all interval
-                    self.corners[curr_cor].append(prev_corners[-1])
-                elif len(next_corners) > len(prev_corners): # If in jth step there were more detected markers
-                    self.corners[curr_cor].append(next_corners[-1])
-
-            i = j
-            j += 1
-
-        for corner in self.corners:
-            print(f'{corner}\n-----')
+        # Traverse through the frames and average smoothen the all 0s
+        for marker in range(2):
+            prev_corner = None
+            for i in range(len(self.corners_np)):
+                if self.corners_np[i,marker,:].all() == 0 and self.corners_np[i-1,marker,:].all() != 0: # The first 0, create a loop
+                    # Find the next non zero index
+                    j = i 
+                    while self.corners_np[j,marker,:].all() == 0:
+                        j += 1
+                    step = (self.corners_np[j,marker,:] - self.corners_np[i-1,marker,:]) / (j-(i-1))
+                    for k in range(i,j):
+                        self.corners_np[k,marker,:] = self.corners_np[k-1,marker,:] + step
+        
+        print(self.corners_np)
         
         last_non_empty_corner = None
         min_x, min_y, max_x, max_y = 0,0,0,0
@@ -156,20 +150,11 @@ class AnimateMarkers:
             if max(self.corners[i][0][0,:,1]) > max_y: 
                 max_y = max(self.corners[i][0][0,:,1])
 
-        # for i,corners in enumerate(self.corners):
-        #     rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners, 0.01,
-        #                                                                self.camera_intrinsics,
-        #                                                                self.distortion_coefficients)
-        #     print('corners:\n{}\nrvec:\n{}\ntvec:\n{}\n----'.format(
-        #         corners, rvec, tvec
-        #     ))
-
         # Set the axes
         num_frames = len(self.corners)
         self.line, = self.axs.plot([], [])
         self.dir = 0
         self.fps = fps
-        # self.pos = np.zeros((len(self.corners), 2)) # This will be filled with forward and side position
         self.axs.set_ylim(min_y, max_y)
         self.axs.set_xlim(min_x, max_x)
         self.axs.set_title("Predicted Markers")
@@ -186,30 +171,26 @@ class AnimateMarkers:
         return self.line,
 
     def animate(self, i):
-        # self.axs.patches = []
-        # for j in range(len(self.corners[i])):
-        #     curr_polygon = self.corners[i][j][0,:]
-        #     if j == 0:
-        #         p = patches.Polygon(curr_polygon, edgecolor='r', facecolor='none')
-        #     else:
-        #         p = patches.Polygon(curr_polygon, edgecolor='g', facecolor='none')
-        #     self.axs.add_patch(p)
-
         # Draw two axes, axes are represented by two arrows starting from the middle of the 
         # rectangles and go to the right top and left corners
+
         self.axs.patches = []
-        for j in range(len(self.corners[i])):
-            curr_polygon = self.corners[i][j][0,:,:]
-            mean_x, mean_y = curr_polygon[:, 0].mean(), curr_polygon[:, 1].mean()
+        for j in range(len(self.corners_np[i])):
+            curr_polygon = self.corners_np[i,j,:]
+            mean_x, mean_y = curr_polygon[:,0].mean(), curr_polygon[:,1].mean()
             right_top_x, right_top_y = curr_polygon[0,0], curr_polygon[0,1]
             right_bot_x, right_bot_y = curr_polygon[1,0], curr_polygon[1,1]
 
-            blue_arr = patches.Arrow(mean_x, mean_y, right_top_x-mean_x, right_top_y-mean_y, color='b')
-            red_arr = patches.Arrow(mean_x, mean_y, right_bot_x-mean_x, right_bot_y-mean_y, color='r')
-            # self.axs.arrow(mean_x, mean_y, right_top_x-mean_x, right_top_y-mean_y, color='b')
-            # self.axs.arrow(mean_x, mean_y, right_bot_x-mean_x, right_bot_y-mean_y, color='r')
-            self.axs.add_patch(blue_arr)
-            self.axs.add_patch(red_arr)
+            if j == 0:
+                blue_arr = patches.Arrow(mean_x, mean_y, right_top_x-mean_x, right_top_y-mean_y, color='b')
+                red_arr = patches.Arrow(mean_x, mean_y, right_bot_x-mean_x, right_bot_y-mean_y, color='r')
+                self.axs.add_patch(blue_arr)
+                self.axs.add_patch(red_arr)
+            else:
+                blue_arr = patches.Arrow(mean_x, mean_y, right_top_x-mean_x, right_top_y-mean_y, color='g')
+                red_arr = patches.Arrow(mean_x, mean_y, right_bot_x-mean_x, right_bot_y-mean_y, color='r')
+                self.axs.add_patch(blue_arr)
+                self.axs.add_patch(red_arr)
 
         return self.line,
 
@@ -228,9 +209,9 @@ if __name__ == "__main__":
         fps = fps
     )
 
-    AnimatePosFrame(
-        data_dir = data_dir, 
-        dump_dir = dump_dir, 
-        dump_file = f'pos_{dump_file}', 
-        fps = fps
-    )
+    # AnimatePosFrame(
+    #     data_dir = data_dir, 
+    #     dump_dir = dump_dir, 
+    #     dump_file = f'pos_{dump_file}', 
+    #     fps = fps
+    # )